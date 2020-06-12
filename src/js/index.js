f2d5eae4d4a5a24146c0f782d5e90f3b1da7711c
Adriana Capińska <jablonska.ad@gmail.com>
2020-06-12 19:23:22 +0200

    scss and js editor

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,23 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const textarea = document.querySelector('#yourText');
+const buttonLoad = document.querySelector('.button-load');
+const buttonSave = document.querySelector('.button-save');
+const text = localStorage.setItem('text','');
+const test = localStorage.getItem('text');
+let result = '';
+
+
+buttonLoad.addEventListener('click', () => {
+    textarea.value = localStorage.getItem('text');
+ 
+})
+
+
+
+buttonSave.addEventListener('click', () => {
+    localStorage.setItem('text', textarea.value);
+})
+
+
